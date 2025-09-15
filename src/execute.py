f3d7f40b7c03f472c545cae933c9d39cf5bf91e3
ArthurMouraa <arthurmoura090@gmail.com>
2025-09-14 23:39:45 -0300

    metodo backup adicionado

--- src/execute.py
+++ src/execute.py
@@ -5,20 +5,15 @@ diretorio = Path("./home/valcann/backupsFrom")
 
 executar = Arquivos(diretorio)
 
-#Listar todos arquivos (nome, tamanho, data de criação, data da última modificação)
-#localizados no caminho /home/valcann/backupsFrom;
+
 lista_dados = executar.extrai_info_arquivos()
 print("lista dados ---->", lista_dados)
 
-#Salvar o resultado no arquivo backupsFrom.log em /home/valcann/;
+
 executar.escreve_em_arquivo(lista_dados,Path("./home/valcann/backupFrom.log"))
 
-#Remover todos os arquivos com data de criação superior a 3 (três) dias;
-executar.deleta_arquivos_por_dia(Path("./home/valcann/backupsFrom"),3)
 
-#Copiar todos os arquivos os arquivos com data de criação menor ou igual a 3 (três) dias
-# em homevalcann/backupsTo;
-# Salvar o resultado no arquivo backupsTo.log em /home/valcann/.
+executar.deleta_arquivos_por_dia(Path("./home/valcann/backupsFrom"),3)
 
 executar.backup_ultimos_por_dias(Path("./home/valcann/backupsTo"), Path("./home/valcann/backUpTo.log"),3)
 
