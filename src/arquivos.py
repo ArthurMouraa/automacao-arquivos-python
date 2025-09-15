155d7fdef58e10702688a003f688846557c17a7d
ArthurMouraa <arthurmoura090@gmail.com>
2025-09-15 00:16:00 -0300

    ajuste em deleta_arquivos_por_dia

--- src/arquivos.py
+++ src/arquivos.py
@@ -47,23 +47,24 @@ class Arquivos:
           log.write(f"Última modificação: {dado['modificado']}\n")
           log.write(" " "\n")
 
-
-
   def deleta_arquivos_por_dia(self, diretorio: Path, dias_atras: int):
-      data_alvo = (datetime.now() - timedelta(days=dias_atras)).date()
-      removidos = 0
-      print(f"**********Procurando arquivos criados em {data_alvo} no diretório {diretorio} \n")
+        limite = (datetime.now() - timedelta(days=dias_atras)).date()
+        removidos = 0
+        print(f"**********Procurando arquivos mais antigos que {limite} no diretório {diretorio}\n")
 
-      for arquivo in diretorio.iterdir():
-          if arquivo.is_file():
-              data_criacao = datetime.fromtimestamp(arquivo.stat().st_birthtime).date()
+        for arquivo in diretorio.iterdir():
+            if arquivo.is_file():
+                data_criacao = datetime.fromtimestamp(arquivo.stat().st_birthtime).date()
 
-              if data_criacao == data_alvo:
-                  print(f"******Removendo: {arquivo} \n")
-                  arquivo.unlink()
-                  removidos += 1
-              else:
-                print(f"Não existe arquivo com essa data -> {data_alvo}")
+                if data_criacao < limite:
+                    print(f"****** Removendo: {arquivo} (criado em {data_criacao})\n")
+                    arquivo.unlink()
+                    removidos += 1
+
+        if removidos == 0:
+            print(f"Não foram encontrados arquivos com mais de {dias_atras} dias.")
+        else:
+            print(f"Total de arquivos removidos: {removidos}")
 
 
   def backup_ultimos_por_dias(self, copia_de:Path, escreve_em:Path, dias:int) :
